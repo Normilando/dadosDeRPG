d3897354613d26cb8dfee3164b44114c0548d973
Normilando <86866114+Normilando@users.noreply.github.com>
2023-03-30 14:57:17 -0300

    Deixando pra tras o onclick() e usando o addEventListener no lugar agora

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-let numeroFinal = document.getElementById("numero-final");
+let numeroFinal = document.querySelector("#numero-final");
 let bordaFundo = document.querySelector('.borda');
+const quatro = document.querySelector('#d4');
+const seis = document.querySelector('#d6');
+const oito = document.querySelector('#d8');
+const dez = document.querySelector('#d10');
+const doze = document.querySelector('#d12');
+const vinte = document.querySelector('#d20');
+const cem = document.querySelector('#d100');
 
-function d4() {
-    let randomNumber = Math.floor( Math.random() * 4 ) + 1
+quatro.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 4 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#A1EFDB";
-}
+})
 
-function d6() {
-    let randomNumber = Math.floor( Math.random() * 6 ) + 1
+seis.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 6 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#EFA1B6";
-}
+})
 
-function d8() {
-    let randomNumber = Math.floor( Math.random() * 8 ) + 1
+oito.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 8 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#B9A1EF";
-}
+})
 
-function d10() {
-    let randomNumber = Math.floor( Math.random() * 10 ) + 1
+dez.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 10 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#D8EFA1";
-}
+})
 
-function d12() {
-    let randomNumber = Math.floor( Math.random() * 12 ) + 1
+doze.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 12 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#FFF63A";
-}
+})
 
-function d20() {
-    let randomNumber = Math.floor( Math.random() * 20 ) + 1
+vinte.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 20 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#FFA33A";
-}
+})
 
-function d100() {
-    let randomNumber = Math.floor( Math.random() * 100 ) + 1
+cem.addEventListener("click", () => {
+    let randomNumber = Math.floor( Math.random() * 100 ) + 1;
     numeroFinal.innerText = randomNumber;
     bordaFundo.style.backgroundColor = "#FF3A3A";
-}
+})
